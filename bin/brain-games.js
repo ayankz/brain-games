6bf7f2db6e2ddd17ee37a3a80650179b248eedd4
ayankz <a.nurinvest@gmail.com>
2020-05-18 02:56:05 +0600

    Js files checked with Linter

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
-import {call} from './cli.js'
+
+import { call } from './cli.js';
+
 console.log('Welcome to the Brain Games!');
 
 call();
--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,6 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
 export const call = () => {
-    const name  = readlineSync.question("May I have your name? ");
-    console.log("Hello, " + name + '!');
-    };
-    
-
-
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};

44229da904ed28b2dae23d5443c4800f5aa7d3eb
ayankz <a.nurinvest@gmail.com>
2020-05-24 23:39:37 +0600

    prepublish step5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import evenStart from '../logic/even.js';
 
 evenStart();
--- bin/cli.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const call = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-};
-export default call;

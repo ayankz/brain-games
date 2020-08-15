83abac40b62327037002860239e941760351af1d
Ayan <a.nurinvest@gmail.com>
2020-08-15 11:26:55 +0600

    final

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,25 +3,24 @@ import getRandomNum from '../random.js';
 
 const description = 'What number is missing in the progression?';
 const maxStep = 6;
-const rowOfnumbersLength = 10;
+const progressionLength = 10;
 const makeProgression = () => {
-  const rowOfnumbers = [];
+  const progression = [];
   const firstNumber = getRandomNum();
   const step = getRandomNum(1, maxStep);
-  
-  for (let i = 0; i < rowOfnumbersLength; i += 1) {
-    rowOfnumbers[i] = firstNumber + i * step;
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression[i] = firstNumber + i * step;
   }
-  const progressions = rowOfnumbers;
-  return progressions;
+
+  return progression;
 };
 
 const getGameData = () => {
-  const progressions = makeProgression();
-  const secretIndex = getRandomNum(0, rowOfnumbersLength);
-  const secretNumber = progressions[secretIndex];
-  progressions[secretIndex] = '..';
-  const question = progressions.join(' ');
+  const progression = makeProgression();
+  const secretIndex = getRandomNum(0, progressionLength);
+  const secretNumber = progression[secretIndex];
+  progression[secretIndex] = '..';
+  const question = progression.join(' ');
   const correctAnswer = secretNumber;
   const gameData = [correctAnswer, question];
   return gameData;
